ea247df52f0637f3a61ebf3ca2ec88df6f105616
Jiahonzheng <jiahonzheng@gmail.com>
2019-12-13 08:14:51 +0800

    fix: 修复 TotalReceipts 中无限请求的问题
    修复 TotalReceipts 中无限请求的问题。

--- Supplier-Financing/Web/src/pages/Receipt/TotalReceipts.tsx
+++ Supplier-Financing/Web/src/pages/Receipt/TotalReceipts.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { formatMessage } from 'umi-plugin-react/locale';
-import { formatCurrency } from '@/utils/utils';
-import request from '@/utils/request';
 import ChartCard from '@/components/ChartCard';
-import { doGetTotalReceipts } from './services';
 import { AppCode, formatAppCode } from '@/utils/enum';
+import { formatCurrency } from '@/utils/utils';
 import { message } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { formatMessage } from 'umi-plugin-react/locale';
+import { doGetTotalReceipts } from './services';
 
 export function TotalReceipts() {
   const [loading, setLoading] = useState(true);
@@ -17,12 +16,11 @@ export function TotalReceipts() {
         // tslint:no-console
         console.error(res.msg, res.data);
         message.error(formatAppCode(res.sub));
-        return;
       }
       setReceipts({ inReceipts: res.data.in_receipts, outReceipts: res.data.out_receipts });
       setLoading(false);
     });
-  });
+  }, []);
 
   return (
     <>
