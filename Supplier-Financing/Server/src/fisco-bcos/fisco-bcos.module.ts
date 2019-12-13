0bc5470af87f70b2dd6ceca409860f3f066855b7
Jiahonzheng <jiahonzheng@gmail.com>
2019-12-13 11:59:44 +0800

    refactor: 删除冗余代码
    删除冗余代码。

--- Supplier-Financing/Server/src/fisco-bcos/fisco-bcos.module.ts
+++ Supplier-Financing/Server/src/fisco-bcos/fisco-bcos.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { FiscoBcosController } from './fisco-bcos.controller';
 import { FiscoBcosService } from './fisco-bcos.service';
 
 @Module({
   imports: [],
-  controllers: [FiscoBcosController],
   providers: [FiscoBcosService],
   exports: [FiscoBcosService],
 })
